38957f91862cb639ab71f85b94c8df166928a93b
James Lockhart <james.lockhart117@gmail.com>
2018-01-31 13:51:18 +0300

    Added a Bee Class that queen and worker wil inherit from

--- Worker/Worker/Queen.cs
+++ Worker/Worker/Queen.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Worker
 {
-    class Queen
+    class Queen : Bee
     {
 
         public Queen(Worker[] workers)
--- Worker/Worker/Worker.cs
+++ Worker/Worker/Worker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Worker
 {
-    class Worker
+    class Worker : Bee
     {
 
         public Worker(string[] jobsICanDo)
