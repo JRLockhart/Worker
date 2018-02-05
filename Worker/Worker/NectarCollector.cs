a8d29124e49378ac37ab83f2174fcd38a42597b4
James Lockhart <james.lockhart117@gmail.com>
2018-02-05 14:36:23 +0300

    Added new classes using Interface objects

---
+++ Worker/Worker/NectarCollector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    class NectarCollector : Worker
+    {
+        public int Nectar { get; set; }
+        public void FindFlowers() { }
+        public void GatherNectar() { }
+        public void ReturnToHive() { }
+    }
+}
---
+++ Worker/Worker/StingPatrol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    class StingPatrol : Worker
+    {
+
+        public int AlertLevel { get; private set; }
+        public int StingerLength { get; set; }
+        public bool SharpenStinger(int Length) { }
+        public bool LookForEnemies() { }
+        public void Sting(string Enemy) { }
+    }
+}
